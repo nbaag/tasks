c72bae7ca4df445e52de3cbe26bba0efb08c0c0f
nbaag <nbaagez@gmail.com>
2022-12-17 19:34:43 +0300

    functions

--- task-1/scripts/main.js
+++ task-1/scripts/main.js
@@ -6,35 +6,37 @@ const button_down = document.getElementById("btn_down");
 let upInterval;
 let downInterval;
 
+function desktopUp() {}
+
 button_up.onclick = function () {
   let top = -tableStyle.top.replace(/\D/g, "");
 
   //desktop
-  if (top === -60) {
+  if (window.screen.width > 420) {
     clearInterval(downInterval);
+
     upInterval = setInterval(() => {
       top -= 1;
-
       table.style.top = top + "px";
 
-      if (top === -120) {
+      if (top <= -120) {
+        table.style.top = -120 + "px";
         clearInterval(upInterval);
       }
     }, 50);
   }
 
   //mobile
-  if (Math.abs(top) === 420) {
+  if (window.screen.width <= 420) {
     top = +tableStyle.top.replace(/\D/g, "");
 
     clearInterval(downInterval);
 
     upInterval = setInterval(() => {
       top -= 1;
-
       table.style.top = top + "px";
 
-      if (Math.abs(top) === 390) {
+      if (top === 390) {
         clearInterval(upInterval);
       }
     }, 50);
@@ -45,32 +47,31 @@ button_down.onclick = function () {
   let top = -tableStyle.top.replace(/\D/g, "");
 
   //desktop
-  if (top === -120) {
+  if (window.screen.width > 420) {
     clearInterval(upInterval);
 
     downInterval = setInterval(() => {
       top += 1;
-
       table.style.top = top + "px";
 
-      if (top === -60) {
+      if (top >= -60) {
+        table.style.top = -60 + "px";
         clearInterval(downInterval);
       }
     }, 50);
   }
 
   //mobile
-  if (Math.abs(top) === 390) {
+  if (window.screen.width <= 420) {
     top = +tableStyle.top.replace(/\D/g, "");
 
     clearInterval(upInterval);
 
     downInterval = setInterval(() => {
       top += 1;
-
       table.style.top = top + "px";
 
-      if (Math.abs(top) === 420) {
+      if (top === 420) {
         clearInterval(downInterval);
       }
     }, 50);
--- task-2/scripts/main.js
+++ task-2/scripts/main.js
@@ -1,32 +1,53 @@
 const yama = document.getElementById("box");
+const yamaStyle = getComputedStyle(yama);
 const circle = document.getElementById("circle");
 
-let yamaTop = 700;
-let yamaHeight = 100;
+let yamaTop = +yamaStyle.top.replace(/\D/g, "");
+let yamaHeight = +yamaStyle.height.replace(/\D/g, "");
 let speed = 1;
-let yamaDown;
-let yamaUp;
 
-const yamaFly = setInterval(() => {
-  yamaTop -= 5;
-  yamaHeight += 5;
-  yama.style.top = yamaTop + "px";
-  yama.style.height = yamaHeight + "px";
+let yamaFly;
 
-  if (yamaTop <= 0) {
-    clearInterval(yamaFly);
-    floatingYama();
-  }
-}, 1);
+function styleFly(top, height, maxTop, fps) {
+  yamaFly = setInterval(() => {
+    yamaTop -= top;
+    yamaHeight += height;
+    yama.style.top = yamaTop + "px";
+    yama.style.height = yamaHeight + "px";
 
-floatingYama = () => {
+    if (yamaTop <= maxTop) {
+      clearInterval(yamaFly);
+      floatingYama();
+    }
+  }, 1000 / fps);
+}
+
+function styleFloat(min, max) {
   setInterval(() => {
     yamaTop += speed;
 
-    if (yamaTop < -2 || yamaTop > 15) {
+    if (yamaTop < min || yamaTop > max) {
       speed = speed * -1;
     }
 
     yama.style.top = yamaTop + "px";
   }, 1000 / 30);
+}
+
+flyingYama = () => {
+  if (window.screen.width > 420) {
+    styleFly(5, 5, 0, 60);
+  } else if (window.screen.width <= 420) {
+    styleFly(3, 5, 800, 30);
+  }
 };
+
+floatingYama = () => {
+  if (window.screen.width > 420) {
+    styleFloat(-2, 15);
+  } else if (window.screen.width <= 420) {
+    styleFloat(790, 800);
+  }
+};
+
+flyingYama();
